02683544a664677937d646620e7a40d9ff33f602
Richard Borges <richard.borges.cc@gmail.com>
2024-08-16 11:20:37 -0300

    chore: understand migrations on back-end with knex

--- versao-inicial/backend/config/db.js
+++ versao-inicial/backend/config/db.js
@@ -1,4 +1,6 @@
 const config = require('../knexfile.js');
 const knex = require('knex')(config);
 
+// WARN: This is not a good to way to start migrations on back-end
+knex.migrate.latest([config]);
 module.exports = knex;
