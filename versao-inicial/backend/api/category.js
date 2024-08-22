f2a17e151979693e2b548485fe5c577074b234d2
Richard Borges <richard.borges.cc@gmail.com>
2024-08-22 09:41:24 -0300

    chore: get user by id, save category

---
+++ versao-inicial/backend/api/category.js
@@ -0,0 +1,36 @@
+module.exports = app => {
+  const { existsOrError, notExistsOrError } = app.api.validation;
+
+  const save = (req, res) => {
+    const category = {
+      ...req.body,
+    };
+
+    if (req.params.id) {
+      category.id = req.params.id;
+    }
+
+    try {
+      existsOrError(category.name, 'Nome não informado');
+    } catch (error) {
+      return res.status(400).send(msg);
+    }
+
+    if (category.id) {
+      app.db('categories')
+        .update(category)
+        .where({ id: category.id })
+        .then(_ => res.status(204).send())
+        .catch(err => res.status(500).send(err));
+    } else {
+      app.db('categories')
+        .insert(category)
+        .then(_ => res.status(204).send())
+        .catch(err => res.status(500).send(err));
+    }
+  }
+
+  return {
+    save,
+  };
+}
--- versao-inicial/backend/api/user.js
+++ versao-inicial/backend/api/user.js
@@ -58,8 +58,18 @@ module.exports = app => {
     .catch(err => res.status(500).send(err));
   }
 
+  const getById = (req, res) => {
+    app.db('users')
+    .select('id', 'name', 'email', 'admin')
+    .where({ id: req.params.id })
+    .first()
+    .then(user => res.json(user))
+    .catch(err => res.status(500).send(err));
+  }
+
   return {
     save,
     get,
+    getById,
   };
 }
